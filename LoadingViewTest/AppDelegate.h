1f9463d1edcc7a8aba569311b692734c6dc48bf5
大竹 雅登 <Masato@myMBP.local>
2013-09-20 22:04:00 +0900

    Initial Commit

---
+++ LoadingViewTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LoadingViewTest
+//
+//  Created by 大竹 雅登 on 13/09/20.
+//  Copyright (c) 2013年 Masato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LoadingViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoadingViewTest
+//
+//  Created by 大竹 雅登 on 13/09/20.
+//  Copyright (c) 2013年 Masato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
